22de917a4eb87d3b48355da176afcfef4c20ebca
EmyDev <115660028+em1dev@users.noreply.github.com>
2025-11-21 20:14:37 +0000

    fix blob appering on top of chat

--- front/src/pages/Landing/styles.ts
+++ front/src/pages/Landing/styles.ts
@@ -111,6 +111,7 @@ export const ChatContainer = styled.div`
 
 // Blob css from https://toruskit.com/tools/blobz/index.html#install
 export const Blob = styled.div`
+  z-index: -1;
   width: 100%;
   height: 100%;
   position: absolute;
