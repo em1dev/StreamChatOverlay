35bd907575771e26f9986b2b4144d5c947422f40
EmyDev <115660028+em1dev@users.noreply.github.com>
2026-04-14 11:42:09 +0100

    typescript fix

---
+++ front/src/globals.d.ts
@@ -0,0 +1,5 @@
+// ./src/globals.d.ts
+// used to fix fontsource import warning issues - https://fontsource.org/docs/getting-started/faq
+declare module '*.css';
+declare module '@fontsource/*' {}
+declare module '@fontsource-variable/*' {}
