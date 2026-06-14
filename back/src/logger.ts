a6b4147ac4ec04a727cefeb420840936f7cb1cec
EmyDev <115660028+em1dev@users.noreply.github.com>
2026-06-14 01:38:11 +0100

    remove authorization header from logs

--- back/src/logger.ts
+++ back/src/logger.ts
@@ -1,8 +1,18 @@
 import pino from 'pino';
 import pinoHttp from 'pino-http';
 
-export const logger = pino();
+export const logger = pino({
+});
 
 export const httpLogger = pinoHttp({
-  base: logger
+  base: logger,
+  serializers: {
+    req: (req) => ({
+      ...req,
+      headers: {
+        ...req.headers,
+        authorization: '**Redacted**'
+      }
+    })
+  }
 });
