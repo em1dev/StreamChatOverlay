570b4c259aed6e82aca71df2f2a52992be1c7050
EmyDev <115660028+em1dev@users.noreply.github.com>
2026-06-29 22:05:23 +0100

    deconstruct settings as to make settings backwards compatible

--- front/src/utils/parseChatSettingsJson.ts
+++ front/src/utils/parseChatSettingsJson.ts
@@ -2,8 +2,18 @@ import { Chat } from '@/api/chatApi/types';
 import { defaultChatSettings } from '@/store/defaultChatSettings';
 import { ChatSettings } from '@/types/settingsTypes';
 
-export const parseChatSettingsJson = (chat: Chat) => {
+export const parseChatSettingsJson = (chat: Chat):ChatSettings => {
   const jsonString = chat.settingsJson;
   if (jsonString.length == 0) return defaultChatSettings;
-  return JSON.parse(jsonString) as ChatSettings;
+  const parsedSettings = JSON.parse(jsonString) as ChatSettings;
+
+  // deconstruct in case we add more values later and the stored config is outdated
+  return {
+    ...defaultChatSettings,
+    ...parsedSettings,
+    ttsConfiguration: {
+      ...defaultChatSettings.ttsConfiguration,
+      ...parsedSettings.ttsConfiguration
+    },
+  };
 };
